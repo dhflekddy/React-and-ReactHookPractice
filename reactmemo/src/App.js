21c3bf02ea030d4349f2a020f330654015231a87
hyojin <dhflekddy@hanmail.net>
2024-03-29 20:28:50 +0900

    useCallback과 React.memo를 사용한 성능개선(useMemo대신 useCallback사용)

--- reactmemo/src/App.js
+++ reactmemo/src/App.js
@@ -1,4 +1,4 @@
-import React, { useMemo } from 'react';
+import React, {useMemo,useCallback } from 'react';
 import {useState} from 'react';
 import Child from './Child';
 
@@ -8,19 +8,20 @@ const App=()=>{
     setParentAge(parentAge+1);
   };
   console.log("부모 컴포넌트가 랜더링되었어요")
-  /**
-   * 잘 알고 있듯이 아래 변수 name은 객체이다. 리랜더링이 될때마다 객체의 주소는 바뀌게 된다. 이에 따라 React.memo를 사용했음에도 불구하고
-   * 자식컴포넌트까지 계속해서 덩달아 랜더링되는 문제가 발생하는 것이다. 이를 해결하기 위해 useMemo를 사용한다.
-   */
-  // const name={
-  //   lastName:'홍',
-  //   firstName:'길동',
+  // const tellMe=()=>{
+  //   console.log("길동아 사랑해~");
   // }
   
-  
-  const name=useMemo(()=>{return {lastName:'홍', firstName:'길동'}}, []);//이렇게 해줌으로써 자식컴포넌트로 전달되는 props가 객체를 담은 변수더라도
-  //불필요한 랜더링이 실행되는 것을 막을 수 있다
+  //값을 반환한느 useMemo
+  // const tellMe=useMemo(()=>{return ()=>{
+  //   console.log("길동아 사랑해~~")
+  // }},[])
 
+  //함수 자체를 반환하는 useCallback
+  const tellMe=useCallback(()=>{
+    console.log("길동아 사랑해~~~")
+  },[])
+ 
   return(
     <div style={{border:'2px solid navy',padding:'10px'}}>
       <h1>부모</h1>
@@ -28,7 +29,7 @@ const App=()=>{
       <button onClick={incrementParentAge}>부모 나이 증가</button>
       {/* 정석대로라면 부모컴포넌트가 랜더링되면서 Child라는 자식컴포넌트까지 랜더링이 순차적으로 이루어지는 것이 정석이다. 하지만 Child컴포넌트에서 React.memo
       를 사용함으로써 자식으로 전달되는 props에 변화가 없다면 랜더링을 막아줄수 있게 된 것이다 */}
-      <Child childName={name} />
+      <Child childName={"홍길동"} tellMe={tellMe}/>
     </div>  
   );
 };
--- reactmemo/src/Child.jsx
+++ reactmemo/src/Child.jsx
@@ -1,11 +1,11 @@
 import React,{memo} from 'react';
-const Child=({childName})=>{
+const Child=({childName, tellMe})=>{
     console.log("자녀 컴포넌트도 랜더링이 되었네요");
     return (
         <div style={{border: '2px solid powderblue', padding:'10px'}}>
             <h3>자녀</h3>
-            <p>성: {childName.lastName}</p>
-            <p>이름:{childName.firstName}</p>
+            <p>이름:{childName}</p>
+            <button onClick={tellMe}>엄마 나 사랑해?</button>
         </div>
     );
 };
